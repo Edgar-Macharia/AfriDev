9f9f1c2d991f5cf2353c1414146443f685be8130
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-09-20 16:31:58 +0300

    add fetching logic

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -1,68 +1,77 @@
-import React, { useContext, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { JobContext } from '../context/JobContext';
+import React, { useState, useEffect } from "react";
+import { JobContext } from "../context/JobContext";
 
 export default function Jobs() {
-  const { jobs, fetchJobs, deleteJob, editJob } = useContext(JobContext);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const LIMIT = 10;
 
-  const { id } = useParams();
-  const singleJob = jobs && jobs.find((job) => job.id === id);
-  console.log(singleJob);
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  // const { id } = useParams();
+  // const singleJob = jobs && jobs.find((job) => job.id === id);
+  // console.log(singleJob);
 
   useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch(
+          `https://api.crackeddevs.com/v1/get-jobs?limit=${LIMIT}`,
+          {
+            headers: {
+              "api-key": `${API_KEY}`,
+            },
+          }
+        );
+        const data = await response.json();
+        setJobs(data);
+        console.log("data: ", data);
+        setLoading(false);
+      } catch (error) {
+        console.log("HTTP-Error: " + error);
+        setLoading(false);
+      }
+    };
     fetchJobs();
-  }, [fetchJobs]);
-
-  const handleEdit = () => {
-    editJob();
-  };
-
-  const handleDelete = () => {
-    deleteJob();
-  };
+  }, [API_KEY]);
 
   return (
-    <>
-      <h1>Jobs</h1>
-      {jobs.length === 0 ? (
-        <p>No jobs available.</p>
+    <div>
+      <h1 className="ms-5">Jobs</h1>
+      {loading ? (
+        <p>Loading...</p>
       ) : (
-        <ul className="list-group mx-5">
-          {jobs.map((job) => (
-            <li
-              key={job.id}
-              className="list-group-item d-flex justify-content-between align-items-start"
-            >
-              <div>
-                <h5>{job.title}</h5>
-                <p>Company: {job.company_name}</p>
-                <p>Location: {job.location}</p>
-                <p>{job.description}</p>
-                <Link
-                  to={`/applyjob/${job.id}`}
-                  className="btn btn-success btn-sm mt-2"
-                >
-                  Apply Job
-                </Link>
+        <div className="container bg-light">
+          <div className="row">
+            {jobs.map((job) => (
+              <div className="col-md-6" key={job.id}>
+                <ul className="list-group mx-5">
+                  <li className="list-card m-2 me-3 list-group-item d-flex justify-content-between align-items-start shadow-sm">
+                    <div>
+                      <span className="">
+                        <h5 className="">{job.title}</h5>
+                      </span>
+                      <p>{job.country_iso || "Remote"}</p>
+                      <p className="text-decoration-underline">{job.company}</p>
+                      {/* <Link
+                      to={`/applyjob/${job.id}`}
+                      className="btn btn-success btn-sm mt-2"
+                    >
+                      Apply Job
+                    </Link> */}
+                    </div>
+                    <div>
+                      <button className="btn btn-success btn-sm me-2">
+                        Apply
+                      </button>
+                    </div>
+                  </li>
+                </ul>
               </div>
-              <div>
-                <button
-                  className="btn btn-success btn-sm me-2"
-                  onClick={() => handleEdit(job.id)}
-                >
-                  Edit
-                </button>
-                <button
-                  className="btn btn-danger btn-sm"
-                  onClick={() => handleDelete(job.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
+            ))}
+          </div>
+        </div>
       )}
-    </>
+    </div>
   );
 }
