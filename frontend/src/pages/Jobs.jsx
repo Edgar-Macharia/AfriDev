dfad71a939fb4b17e5403d1b35989722e655315a
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-10-03 15:12:45 +0300

    set pagination

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-
-
 export default function Jobs() {
   const API_KEY = process.env.REACT_APP_API_KEY;
   const LIMIT = 10;
 
   const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
-
+  const [totalJobs, setTotalJobs] = useState(100);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchJobs = async () => {
       try {
         const response = await fetch(
-          `https://api.crackeddevs.com/v1/get-jobs?limit=${LIMIT}`,
+          `https://api.crackeddevs.com/v1/get-jobs?page=${currentPage}`,
           {
             headers: {
               "api-key": `${API_KEY}`,
@@ -32,9 +31,15 @@ export default function Jobs() {
       }
     };
     fetchJobs();
-  }, [API_KEY]);
+  }, [API_KEY, currentPage]);
 
-  
+  const totalPages = Math.ceil(totalJobs / LIMIT);
+
+  const handlePageChange = (page) => {
+    if (page > 0 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
 
   return (
     <div>
@@ -47,22 +52,83 @@ export default function Jobs() {
             {jobs.map((job) => (
               <div className="col-md-6" key={job.id}>
                 <ul className="list-group mx-5">
-                  <Link to={`/applyJob/${job.id}`} className="list-card m-2 me-3 list-group-item d-flex justify-content-between align-items-start shadow-sm">
+                  <Link
+                    to={`/applyJob/${job.id}?page=${currentPage}`}
+                    className="list-card m-2 me-3 list-group-item d-flex justify-content-between align-items-start shadow-sm"
+                  >
                     <div>
                       <span className="d-flex justify-content-start">
-                        <img className="h-25 w-25 object-fit border-rounded" src={job.logo_url} alt="logo"></img>
+                        <img
+                          className="h-25 w-25 object-fit border-rounded"
+                          src={job.logo_url}
+                          alt="logo"
+                        ></img>
                         <h5 className="pt-4 ps-2">{job.title}</h5>
                       </span>
                       <p>{job.country_iso || "Remote"}</p>
                       <span className="d-flex justify-content-between">
-                      <p className="text-decoration-underline">{job.company}</p>
-                      <p><small className="text-body-secondary">{new Date(job.created_at).toLocaleDateString()}</small></p>
+                        <p className="text-decoration-underline">
+                          {job.company}
+                        </p>
+                        <p>
+                          <small className="text-body-secondary">
+                            {new Date(job.created_at).toLocaleDateString()}
+                          </small>
+                        </p>
                       </span>
                     </div>
                   </Link>
                 </ul>
               </div>
             ))}
+          </div>
+          <div>
+            <nav
+              aria-label="Page-navigation"
+              className="d-flex justify-content-center"
+            >
+              <ul className="pagination">
+                <li
+                  className={`page-item ${currentPage === 1 ? "disabled" : ""}`}
+                >
+                  <button
+                    className="page-link"
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}
+                  >
+                    Prev
+                  </button>
+                </li>
+                {[...Array(totalPages)].map((_, pageIndex) => (
+                  <li
+                    key={pageIndex}
+                    className={`page-item ${
+                      pageIndex + 1 === currentPage ? "active" : ""
+                    }`}
+                  >
+                    <button
+                      className="page-link"
+                      onClick={() => handlePageChange(pageIndex + 1)}
+                    >
+                      {pageIndex + 1}
+                    </button>
+                  </li>
+                ))}
+                <li
+                  className={`page-item ${
+                    currentPage === totalPages ? "disabled" : ""
+                  }`}
+                >
+                  <button
+                    className="page-link"
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    disabled={currentPage === totalPages}
+                  >
+                    Next
+                  </button>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       )}
