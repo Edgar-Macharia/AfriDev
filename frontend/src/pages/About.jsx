b3cca3c686a4ac013e33cd9ded09af5b54003da3
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-10-04 15:29:34 +0300

    fix cards styling

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,66 +1,53 @@
 import React from "react";
+import img1 from "../images/Hiring-rafiki (2).png"
+import img2 from "../images/New team members-bro.png";
+import img3 from "../images/Remote team-bro.png";
 
 function About() {
   return (
-    <div className="my-3 bg-success bg-opacity-50">
-      <div className="container text-center mb-4">
-        <div className="row">
-          <div className="col-md-8 d-flex align-items-center">
-            Search and Hire Full-time Staff or Freelancers
-            <br /> Easily hire talent for your short-term or long-term projects.
-            <br />
-            Search and instantly get matched with candidates that meet your
-            specifications and budget.
-          </div>
-          <div className="col-6 col-md-4 mt-5">
-            <img
-              className="img-fluid bg-light"
-              src="https://i2.wp.com/sendtask.io/wp-content/uploads/2017/12/freelance-websites.jpg?fit=5000%2C3000&ssl=1"
-              alt="developer"
-            />
-          </div>
+    <div className="my-3 py-3 bg-success bg-opacity-50">
+    <div className="container">
+      <div className="row align-items-center text-center text-md-start">
+        <div className="col-md-8 mb-md-0">
+          <h2 className="mb-3">Search and Find Your Ideal Remote Developer Role</h2>
+          <p className="mb-2">Discover full-time positions and freelance opportunities tailored to your skills and experience.</p>
+          <p>Get instantly matched with projects that fit your expertise and desired compensation.</p>
+        </div>
+        <div className="col-md-4">
+          <img className="img-fluid" src={img1} alt="job-hunt-img" />
         </div>
       </div>
+      
       <hr/>
-
-      <div className="container text-center mb-4 pt-5">
-        <div className="row">
-          <div className="col-6 col-md-4 my-3">
-            <img
-              className="img-fluid bg-light"
-              src="https://www.webfox.dev/assets/img/blog/A-Guide-to-Freelancer-Marketplace-development-like-Fiverr-and-Upwork/How-does-the-freelancer-marketplace-help-Employers.png"
-              alt="developer"
-            />
-          </div>
-          <div className="col-md-8 d-flex align-items-center">
-            Schedule an interview with the talent and hire them.
-            <br />
-            Sign a contract that details the project scope and get started.
-          </div>
+      
+      <div className="row align-items-center ps-5 text-center text-md-start">
+        <div className="col-md-4 mb-md-0 ps-5">
+          <img className="img-fluid" src={img2} alt="developer" />
+        </div>
+        <div className="col-md-8 ps-5">
+          <h2 className="mb-3">Seamless Application Process</h2>
+          <p className="mb-2">Apply with ease, schedule interviews directly, and secure your next role quickly.</p>
+          <p>Clear project scopes and contracts ensure you know exactly what to expect.</p>
         </div>
       </div>
+  
       <hr/>
-
-      <div className="container text-center mb-3 pt-5">
-        <div className="row">
-          <div className="col-md-4 d-flex align-items-center mb-3 mb-md-0">
-            Our talents are remote-ready, fluent in collaborative tools, and
-            available for your entire project.
-          </div>
-          <div className="col-md-4 mb-3 mb-md-4">
-            <img
-              className="img-fluid bg-light"
-              src="https://i0.wp.com/geeky.com.ng/wp-content/uploads/2023/02/money-g0e4aa43d8_1280.png?fit=1280%2C915&ssl=1"
-              alt="developer"
-            />
-          </div>
-          <div className="col-md-4 d-flex align-items-center">
-            Our multilingual and multinational professional staff will also be
-            available at any time to support you.
-          </div>
+  
+      <div className="row align-items-stretch text-center">
+        <div className="col-md-4 mb-md-0 d-flex flex-column justify-content-center">
+          <p>All positions are remote-friendly, perfect for developers fluent in collaborative tools 
+          and ready to contribute to projects from anywhere in the world</p>
+        </div>
+        <div className="col-md-4 mb-md-0">
+          <img className="img-fluid h-100 object-fit-cover" src={img3} alt="remote-team" />
+        </div>
+        <div className="col-md-4 d-flex flex-column justify-content-center">
+          <p>Our multilingual and multinational professional staff will also be
+          available at any time to support you.</p>
         </div>
       </div>
     </div>
+  </div>
   );
 }
 
